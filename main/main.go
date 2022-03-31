813f49e3d226b1807698589b4daf2f57361fa272
user7652314326 <raphael.pietrzak@ynov.com>
2022-03-31 11:41:58 +0200

    récup

--- main/main.go
+++ main/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/accueil", f.MainPage())
+	http.HandleFunc("/accueil", f.MainPage)
 
 	//Show #CSS
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("./views"))
+	http.Handle("/views/", http.StripPrefix("/view/", fs))
 
 	fmt.Println("Listening at http://localhost:5500")
 	http.ListenAndServe("localhost:5500", nil)
