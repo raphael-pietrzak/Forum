5700a6447e8c5ff5c7052bfaf11b828becc4828b
user7652314326 <raphael.pietrzak@ynov.com>
2022-03-31 17:15:46 +0200

    debut de database

--- main/main.go
+++ main/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	f "forum"
 	"net/http"
+	"strconv"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
+func debug(err error) {
+	if err!= nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 
+	db, _ := sql.Open("sqlite3", "./datbase.db")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT);")
+	debug(err)
+	db.Exec("INSERT IF NOT EXISTS INTO people (firstname, lastname) VALUES ('ROB', 'STARK');")
+	rows, err  := db.Query("SELECT id, firstname, lastname FROM people;")
+	debug(err)
+	var id int
+	var firstname string
+	var lastname string
+	for rows.Next(){
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id)+" "+firstname+" "+lastname)
+	}
 	http.HandleFunc("/", f.Forum)
 	http.HandleFunc("/addpost", f.AddPost)
 	http.HandleFunc("/login", f.Login)
