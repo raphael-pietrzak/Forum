34e439ad7e62c47b926df10b326fc73fc81ae2df
Max Doublait <maxdoublait@Maxs-MacBook-Pro.local>
2022-04-01 10:08:12 +0200

    structure Post database

--- addToTable.go
+++ addToTable.go
@@ -26,6 +26,52 @@ func RecupUser() []User {
 	debug(err)
 	var newTab []User
 	for rows.Next() {
+		var uid int
+		var username string 
+		var email string 
+		var passwd string 
+		err = rows.Scan(&uid, &username, &email, &passwd) 
+		debug(err)
+		newTab = append(newTab, User{ Uid: uid, Email:email, Username:username, Passwd:passwd})
+	}
+	err = rows.Err()
+	debug(err)
+	return newTab 
+}
+
+func RecupPost() []Post {
+	db, err := sql.Open("sqlite3", "./database.db")
+	selection := "SELECT * FROM post" 
+	rows, err := db.Query(selection)
+	debug(err)
+
+	err = rows.Err()
+	debug(err)
+	var newTab []Post
+	for rows.Next() {
+		var pid int
+		var category string 
+		var picture string 
+		var comment []string 
+		err = rows.Scan(&pid, &category, &picture, &comment) 
+		debug(err)
+		newTab = append(newTab, Post{Pid: pid, Category:category, Picture:picture, Comment:comment})
+	}
+	err = rows.Err()
+	debug(err)
+	return newTab 
+}
+
+func RecupComment() []Comment {
+	db, err := sql.Open("sqlite3", "./database.db")
+	selection := "SELECT * FROM comment" 
+	rows, err := db.Query(selection)
+	debug(err)
+
+	err = rows.Err()
+	debug(err)
+	var newTab []User
+	for rows.Next() {
 		var username string 
 		var email string 
 		var passwd string 
@@ -38,4 +84,3 @@ func RecupUser() []User {
 	debug(err)
 	return newTab 
 }
-
