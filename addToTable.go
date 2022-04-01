44425e0aebfab7d4827430bb2fb63d8f5d163290
Max Doublait <maxdoublait@Maxs-MacBook-Pro.local>
2022-04-01 09:57:57 +0200

    structure User database

---
+++ addToTable.go
@@ -0,0 +1,41 @@
+package forum 
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	_ "log"
+)
+
+
+func addValues(table string, types string, value string) {
+	db, err := sql.Open("sqlite3", "./database.db")
+	debug(err)
+	execution := "INSERT IF NOT EXISTS INTO " + table + "('"+ types + "') VALUES (" + "'"+ value +"');"
+	db.Exec(execution);
+
+
+}
+
+func RecupUser() []User {
+	db, err := sql.Open("sqlite3", "./database.db")
+	selection := "SELECT * FROM user" 
+	rows, err := db.Query(selection)
+	debug(err)
+
+	err = rows.Err()
+	debug(err)
+	var newTab []User
+	for rows.Next() {
+		var username string 
+		var email string 
+		var passwd string 
+		var uid int
+		err = rows.Scan(&uid, &username, &email, &passwd) 
+		debug(err)
+		newTab = append(newTab, User{ Uid: uid, Email:email, Username:username, Passwd:passwd})
+	}
+	err = rows.Err()
+	debug(err)
+	return newTab 
+}
+
