9a98be84195b00ff561c73896b7ecf8a6073606b
ALATORRE <audran.latorre@ynov.com>
2022-04-21 09:45:58 +0200

    ajout ligne confirmation mot de passe

--- addUser.go
+++ addUser.go
@@ -16,15 +16,18 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		tmpl := template.Must(template.ParseFiles("static/sign_up.html"))
 		tmpl.Execute(w, Data)
+		Data.ErrorMessage = ""
 
 	case "POST":
 		db, _ := sql.Open("sqlite3", "./database.db")
 
 		ErrParseForm(w, r)
+		Data.ErrorMessage = ""
 
 		username := r.Form.Get("user_name")
 		mail := r.Form.Get("mail")
 		password := r.Form.Get("password")
+		password_confirm := r.Form.Get("confirm_password")
 		uuid := uuid.New()
 		avatar := "user.png"
 		user := "user"
@@ -34,6 +37,11 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 			user = "admin"
 		}
 
+		fmt.Println(mail)
+		fmt.Println(password)
+		fmt.Println(password_confirm)
+
+
 		AlreadyUsed := false
 		for _, v := range Users {
 			fmt.Println(v.Email)
@@ -42,13 +50,22 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 				Data.ErrorMessage = "Email déjà utilisé"
 			}
 		}
-		if AlreadyUsed  {
+
+		if AlreadyUsed && password != password_confirm {
+			Data.ErrorMessage = "Email déjà utilisé et mot de passe incorect"
+			http.Redirect(w, r, "/adduser", 301)
+		} else if password != password_confirm {
+			Data.ErrorMessage = "Not same password"
 			http.Redirect(w, r, "/adduser", 301)
 		} else {
-			_, err := db.Exec(`INSERT INTO user ('uid', 'username', 'email', 'passwd', 'avatar', 'type') VALUES (?, ?, ?, ?, ?, ?)`, uuid.String(), username, mail, Hash(password), avatar, user)
-			Debug(err)
-			CreateCookie(w, r, uuid.String())
-			http.Redirect(w, r, "/", 301)
+			if AlreadyUsed  {
+				http.Redirect(w, r, "/adduser", 301)
+			} else {
+				_, err := db.Exec(`INSERT INTO user ('uid', 'username', 'email', 'passwd', 'avatar', 'type') VALUES (?, ?, ?, ?, ?, ?)`, uuid.String(), username, mail, Hash(password), avatar, user)
+				Debug(err)
+				CreateCookie(w, r, uuid.String())
+				http.Redirect(w, r, "/", 301)
+			}
 		}
 
 	}
